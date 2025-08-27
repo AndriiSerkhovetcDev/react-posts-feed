1d3d17b5ccabefcc21240e65dc83a2a42842340b
AndriiSerkhovetcDev <andrii.serkhovetc.dev@gmail.com>
2025-08-27 16:18:08 +0300

    Revert "fetch posts and render them on page"
    This reverts commit 1733702c6ee99f5f593ad1f02420bbef133e265c.

--- src/api/api.js
+++
@@ -1,6 +0,0 @@
-export async function fetchPosts () {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    if (!res.ok) throw new Error("Failed to fetch posts");
-
-    return res.json();
-}
